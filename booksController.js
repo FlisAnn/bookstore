2ea611ed7521de3ef633ac7f3297708e2a89185d
flis_ann <ann.flismark@gmail.com>
2021-01-20 11:41:52 +0100

    Adding update and delete action

--- booksController.js
+++ booksController.js
@@ -3,28 +3,43 @@ const { pool } = require("./config");
 const booksController = {
   async index(request, response, next) {
     const { rows } = await pool.query("SELECT * FROM books");
-    response
-      .status(200)
-      .json({ books: rows });
+    response.status(200).json({ books: rows });
   },
 
   async create(request, response, next) {
     const { author, title } = request.body;
-    await pool.query("INSERT INTO books (author, title) VALUES ($1, $2)", 
-    [
+    await pool.query("INSERT INTO books (author, title) VALUES ($1, $2)", [
       author,
       title,
     ]);
-    response
-      .status(201)
-      .json({ message: "The book was successfully added" });
+    response.status(201).json({ message: "The book was successfully added" });
   },
   async show(request, response, next) {
     const { id } = request.params;
-    const { rows } = await pool.query("SELECT * FROM books WHERE id = $1 LIMIT 1", [id]);
+    const {
+      rows,
+    } = await pool.query("SELECT * FROM books WHERE id = $1 LIMIT 1", [id]);
+    response.status(200).json({ book: rows[0] });
+  },
+  async delete(request, response, next) {
+    const { id } = request.params;
+    await pool.query("DELETE FROM books WHERE id = $1", [id]);
+    response.status(202).json({ message: "The book was removed" });
+  },
+  async update(request, response, next) {
+    const { author, title } = request.body;
+    const { id } = request.params;
+    const { rows } = await pool.query(
+      `UPDATE books 
+      SET author = $1, title = $2
+      WHERE id = $3
+      RETURNING *
+      `,
+      [author, title, id]
+    );
     response
-      .status(200)
-      .json({book: rows[0]})
+      .status(201)
+      .json({ message: "The book was successfully updated", book: rows[0] });
   },
 };
 
--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app
 app
   .route('/books/:id')
   .get(booksController.show)
+  .delete(booksController.delete)
+  .put(booksController.update)
 app.listen(3001, () => {
   console.log("Server is up and running..");
 });
