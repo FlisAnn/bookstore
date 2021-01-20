09baa6933f412a6974549860449a76cc30fb6b92
flis_ann <ann.flismark@gmail.com>
2021-01-20 10:46:50 +0100

    Refactoring crudaction into it's own controller

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { pool } = require("./config");
+const {booksController} = require('./booksController')
 
 const app = express();
 
@@ -9,33 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const getBooks = (request, response, next) => {
-  pool.query("SELECT * FROM books", (error, results) => {
-    if (error) {
-      throw error;
-    } else {
-      response.status(200).json({ books: results.rows });
-    }
-  });
-};
-
-const addBook = (request, response, next) => {
-  const { author, title } = request.body;
-  pool.query(
-    "INSERT INTO books (author, title) VALUES ($1, $2)",
-    [author, title],
-    error => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).json({message: 'The book was successfully added'})
-    });
-};
-
 app
 .route("/books")
-.get(getBooks)
-.post(addBook);
+.get(booksController.index)
+.post(booksController.create);
 
 app.listen(3001, () => {
   console.log("Server is up and running..");
